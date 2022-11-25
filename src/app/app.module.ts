8fd661b4e8e18db94fa08f22915dd4bfa0a31aea
Asiyacm <asiyacmuhammed@gmail.com>
2022-11-25 13:10:47 +0530

    add commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddtodoListComponent } from './addtodo-list/addtodo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddtodoListComponent
   ],
   imports: [
     BrowserModule,
