ed55262caa69c4ffd2c71970c7a19c4ccc4e2f53
Asiyacm <asiyacmuhammed@gmail.com>
2022-11-25 13:47:55 +0530

    view commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddtodoListComponent } from './addtodo-list/addtodo-list.component';
+import { ViewtodoListComponent } from './viewtodo-list/viewtodo-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes=[
+  {
+    path: "",
+    component:AddtodoListComponent
+  },
+  {
+    path:"view",
+    component:ViewtodoListComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddtodoListComponent
+    AddtodoListComponent,
+    ViewtodoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
